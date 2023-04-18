f310fc8337e264eaccbf7ecc4fccf5ef804db5d6
harsh0641 <72370572+harsh0641@users.noreply.github.com>
2023-04-18 12:49:11 +0530

    first commit

---
+++ first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+   // printf() displays the string inside quo5tation
+   printf("Enter the vaue :");
+   int a;
+   scanf("%d",a);
+   printf("your Value is %d " ,a);
+
+   return 0;
+}
